d3a3296ace80dbc1756b69dab08637eb78f6a7e9
Sayan Pal <spal@netatwork.de>
2021-03-06 14:17:10 +0100

    added another example with a smaller image

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { customElement } from '@aurelia/runtime-html';
 import template from './app.html';
 import img from "../images/pexels-pixabay-459225.jpg";
+import smallImg from "../images/pexels-stein-egil-liland-1933239.jpg";
 
 @customElement({ name: 'app', template })
 export class App {
   public readonly message: string = 'Hello World!';
   private readonly img = img;
+  private readonly smallImg = smallImg;
 }
